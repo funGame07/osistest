eb7d01a18f5335c5f73cb9b4dfe6a4c972a8bb13
elbert <elbertchen007@gmail.com>
2024-09-29 12:05:16 +0700

    update ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,12 +7,19 @@ import {
   keyframes,
   StackDivider,
   Button,
-  Link
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverBody,
+  PopoverContent,
+  PopoverHeader,
+  PopoverCloseButton
  } from "@chakra-ui/react"
  import { FaExchangeAlt } from "react-icons/fa";
 import Carousel from "./Carousel";
 import { useContext } from "react";
 import { osis } from "../App";
+import { IoIosArrowBack } from "react-icons/io";
 
 import "./home.css"
 
@@ -54,29 +61,11 @@ function Home() {
         
       </Flex>
 
-      <Flex alignItems={"center"} pos={"relative"} minW={"full"} gap={4} px={4} py={3} pt={9}>
-        <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
-          <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
-        </Box>
-        <Text className="font-link" fontSize={{base: "lg", lg:"sm"}} fontWeight={700}>Anggota</Text>
-        <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
-          <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
-        </Box>
-      </Flex>
-
+      <Division text={"Anggota"} colorMode={colorMode} pt={9}/>
       
-        <Carousel />
+      <Carousel />
 
-
-        <Flex alignItems={"center"} pos={"relative"} minW={"full"} gap={4} px={4} py={3} pt={9}>
-          <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
-            <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
-          </Box>
-          <Text className="font-link" fontSize={{base: "lg", lg:"sm"}} fontWeight={700}>Others</Text>
-          <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
-            <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
-          </Box>
-        </Flex>
+      <Division text={"Lainnya"} colorMode={colorMode} pt={9}/>
 
           {/* others */}
       <Flex gap={4} flexDir={'column'} minW={"full"}>
@@ -86,18 +75,42 @@ function Home() {
               <Text fontWeight={700} color={"white"} textAlign={"center"}>01</Text>
             </Circle>
           </Circle>
+          <Popover placement="top-end">
+            <PopoverTrigger>
+              <Box bg={"red"} flexGrow={1} minH={"110px"} maxH={"130px"} 
+              rounded={"xl"} pos={"relative"} p={5} bgImage={"visi.png"}
+              bgSize={"cover"} bgPos={"center"} bgRepeat={"no-repeat"}
+              filter={"brightness(0.7)"} cursor={"pointer"}>
+                <Text color={"white"} textShadow={"-2px -2px 20px black"}
+                fontSize={"lg"} fontWeight={"800"} fontFamily={"sans-serif"}
+                pos={"absolute"} w={"63%"} loading="lazy">
+                  Visi dan Misi Kami
+                </Text>
+                <Text color={"white"} pos={"absolute"} bottom={3} fontWeight={600} letterSpacing={2}>Yuk disimak!</Text>
+            </Box>
+            </PopoverTrigger>
+            <PopoverContent border={"2px solid teal"}>
+              <PopoverHeader height={"55px"}>
+                <PopoverCloseButton size={40} left={-200}>
+                  <IoIosArrowBack size={"25px"} cursor={"pointer"}/>
+                  <Image src={colorMode == "light" ? "logoosis-black.png" : "logoosis-white.png"} maxW={"20"}/>
+                </PopoverCloseButton>
+              </PopoverHeader>
+              <PopoverBody>
+                <Division text={"Visi"} colorMode={colorMode} pt={0} py={1}/>
+                <Text className="font-link" fontSize={"sm"}>1. blablablablablablabalbl</Text>
+                <Text className="font-link" fontSize={"sm"}>2. xixixixixixixixiixxixi</Text>
+                <Text className="font-link" fontSize={"sm"}>3. blablablablablablabalbl</Text>
+                <Text className="font-link" fontSize={"sm"}>4. xixixixixixixixiixxixi</Text>
+                <Text className="font-link" fontSize={"sm"}>5. blablablablablablabalbl</Text>
+                <Text className="font-link" fontSize={"sm"}>6. xixixixixixixixiixxixi</Text>
 
-          <Box bg={"red"} flexGrow={1} minH={"110px"} maxH={"130px"} 
-          rounded={"xl"} pos={"relative"} p={5} bgImage={"visi.png"}
-          bgSize={"cover"} bgPos={"center"} bgRepeat={"no-repeat"}
-          filter={"brightness(0.7)"} cursor={"pointer"}>
-            <Text color={"white"} textShadow={"-2px -2px 20px black"}
-            fontSize={"lg"} fontWeight={"800"} fontFamily={"sans-serif"}
-            pos={"absolute"} w={"63%"} loading="lazy">
-              Visi dan Misi Kami
-            </Text>
-            <Text color={"white"} pos={"absolute"} bottom={3} fontWeight={600} letterSpacing={2}>Yuk disimak!</Text>
-          </Box>
+                <Division text={"Misi"} colorMode={colorMode} pt={7} py={0}/>
+                <Text className="font-link" fontSize={"sm"}>1. blablablablablablabalbl</Text>
+                <Text className="font-link" fontSize={"sm"}>2. xixixixixixixixiixxixi</Text>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
         </Flex>
 
         <Flex width={"full"} px={5} gap={5}>
@@ -107,17 +120,33 @@ function Home() {
             </Circle>
           </Circle>
 
-          <Box bg={"red"} flexGrow={1} minH={"110px"} maxH={"130px"} 
-          rounded={"xl"} pos={"relative"} p={5} bgImage={"lomba.png"}
-          bgSize={"cover"} bgPos={"center"} bgRepeat={"no-repeat"}
-          filter={"brightness(0.7)"} cursor={"pointer"}>
-            <Text color={"white"} textShadow={"-2px -2px 20px black"}
-            fontSize={"lg"} fontWeight={"800"} fontFamily={"sans-serif"}
-            pos={"absolute"} w={"63%"} loading="lazy">
-              Dokumentasi Lomba
-            </Text>
-            <Text color={"white"} pos={"absolute"} bottom={3} fontWeight={600} letterSpacing={2}>Yuk saksikan!</Text>
-          </Box>
+          <Popover placement="top-end">
+            <PopoverTrigger>
+              <Box bg={"red"} flexGrow={1} minH={"110px"} maxH={"130px"} 
+              rounded={"xl"} pos={"relative"} p={5} bgImage={"lomba.png"}
+              bgSize={"cover"} bgPos={"center"} bgRepeat={"no-repeat"}
+              filter={"brightness(0.7)"} cursor={"pointer"}>
+                <Text color={"white"} textShadow={"-2px -2px 20px black"}
+                fontSize={"lg"} fontWeight={"800"} fontFamily={"sans-serif"}
+                pos={"absolute"} w={"63%"} loading="lazy">
+                  Dokumentasi Lomba
+                </Text>
+                <Text color={"white"} pos={"absolute"} bottom={3} fontWeight={600} letterSpacing={2}>Yuk disimak!</Text>
+            </Box>
+            </PopoverTrigger>
+            <PopoverContent border={"2px solid teal"}>
+              <PopoverHeader height={"55px"}>
+                <PopoverCloseButton size={40} left={-200}>
+                  <IoIosArrowBack size={"25px"} cursor={"pointer"}/>
+                  <Image src={colorMode == "light" ? "logoosis-black.png" : "logoosis-white.png"} maxW={"20"}/>
+                </PopoverCloseButton>
+              </PopoverHeader>
+              <PopoverBody>
+                
+                
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
         </Flex>
       </Flex>
       
@@ -129,6 +158,20 @@ function Home() {
       
 
     </Box>
+  )
+}
+
+function Division({text, colorMode, pt, py=3}){
+  return (
+    <Flex alignItems={"center"} pos={"relative"} minW={"full"} gap={4} px={4} py={py} pt={pt}>
+      <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
+        <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
+      </Box>
+      <Text className="font-link" fontSize={{base: "lg", lg:"sm"}} fontWeight={700}>{text}</Text>
+      <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
+        <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
+      </Box>
+    </Flex>
   )
 }
 
