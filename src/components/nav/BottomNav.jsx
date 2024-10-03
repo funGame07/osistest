6d48f61f7688c4853be53e687692729d6e9b3d7b
FunGame <161847205+funGame07@users.noreply.github.com>
2024-10-03 11:38:42 +0700

    Update BottomNav.jsx

--- src/components/nav/BottomNav.jsx
+++ src/components/nav/BottomNav.jsx
@@ -1,67 +1,49 @@
 import { 
     Box,
     Text,
-    Button
+    Button,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverArrow
  } from '@chakra-ui/react'
 import { Link } from 'react-router-dom';
 import "./navbar.css"
-import React from 'react'
+import React, { useContext }from 'react'
 import { GoHome } from "react-icons/go";
 import { CiSquarePlus } from "react-icons/ci";
 import { GrSearchAdvanced } from "react-icons/gr";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { HiOutlineTrophy } from "react-icons/hi2";
+import { osis } from '../../App';
+import BottomNavContent from './BottomNavContent';
 
 
-function BottomNav({colorMode}) {
-
+function BottomNav() {
+    const {colorMode, showBottomNavbar, setShowBottomNavbar} = useContext(osis)
 // bgGradient={"linear(to-b, #302312, #4c320f)"}
 // bgGradient={"linear(to-r, #241a0e, #302312, #302312)"}
 
   return (
-    <Box className='bottom-nav' position={"fixed"} bgGradient={colorMode == "light"? "linear(to-b, gray.100, white)" : "linear(to-b, black, gray.800)"} zIndex={9999}>
-        <Box className='nav' px={{base: 4, lg: 3}} gap={{base: 7, lg: 5}} py={{base: 0.5, lg:1}} display={"flex"} alignItems={{base: "center", lg: "start"}}
-        borderRadius={"xl"} fontWeight={900} h={{base: "fit-content", lg: "40px"}} bgGradient={colorMode == "light"? "linear(to-r, gray.100, white, gray.100)" : "linear(to-r, black, gray.800, black)"}>
-        <Link to={"/"}>
-            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
-                <GoHome size={23} className='navicon'/>
-                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
-                    Home
-                </Text>
-            </Box>
-        </Link>
-
-        <Link to={"/explore"}>
-            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
-                <GrSearchAdvanced size={23} className='navicon' />
-                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
-                    Explore
-                </Text>
-            </Box>
-        </Link>
-
-        <Link to={"/admin"}>
-            <CiSquarePlus size={40} className='navicon-mid' cursor={"pointer"}/>
-        </Link>
-
-        <Link to={"/voting"}> 
-            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
-                <IoNotificationsOutline size={23} className='navicon'/>
-                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
-                    Voting
-                </Text>
-            </Box>
-        </Link>
-
-        <Link to={"/event"}>
-            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
-                <HiOutlineTrophy size={23} className='navicon'/>
-                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
-                    Event
-                </Text>
-            </Box>
-        </Link>
-        </Box>
+    <Box className='bottom-nav' position={"fixed"} 
+    bgGradient={colorMode == "light"? "linear(to-b, gray.100, white)" : "linear(to-b, black, gray.800)"} 
+    zIndex={9999} 
+    display={{base: showBottomNavbar ? "" : "none", lg: ""}}
+    >
+        <BottomNavContent />
+        {/* <Box>
+            <Popover placement='right'>
+                <PopoverTrigger>
+                    <Button display={{base: "none", lg: "block"}} top={-5} right={0}>
+                        <CiSquarePlus /> 
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <BottomNavContent />
+                </PopoverContent>
+            </Popover>
+        </Box> */}
+        
     </Box>
   )
 }
