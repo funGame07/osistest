374ccfac3514596a745add3a86994c197d22b059
FunGame <161847205+funGame07@users.noreply.github.com>
2024-10-03 11:40:19 +0700

    Create ButtonNavContent.jsx

---
+++ src/components/nav/ButtonNavContent.jsx
@@ -0,0 +1,64 @@
+import { 
+    Box,
+    Text,
+    Button
+ } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
+import "./navbar.css"
+import React, { useContext }from 'react'
+import { GoHome } from "react-icons/go";
+import { CiSquarePlus } from "react-icons/ci";
+import { GrSearchAdvanced } from "react-icons/gr";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { HiOutlineTrophy } from "react-icons/hi2";
+import { osis } from '../../App';
+
+function BottomNavContent() {
+    const {colorMode, showBottomNavbar, setShowBottomNavbar} = useContext(osis)
+  return (
+    <Box className='nav' px={{base: 4, lg: 3}} gap={{base: 7, lg: 5}} py={{base: 0.5, lg:1}}  alignItems={{base: "center", lg: "start"}} display={{base: "flex", lg: "none"}}
+        borderRadius={"xl"} fontWeight={900} h={{base: "fit-content", lg: "40px"}} bgGradient={colorMode == "light"? "linear(to-r, gray.100, white, gray.100)" : "linear(to-r, black, gray.800, black)"}>
+        <Link to={"/"}>
+            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
+                <GoHome size={23} className='navicon'/>
+                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
+                    Home
+                </Text>
+            </Box>
+        </Link>
+
+        <Link to={"/explore"}>
+            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
+                <GrSearchAdvanced size={23} className='navicon' />
+                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
+                    Explore
+                </Text>
+            </Box>
+        </Link>
+
+        <Link to={"/admin"}>
+            <CiSquarePlus size={40} className='navicon-mid' cursor={"pointer"}/>
+        </Link>
+
+        <Link to={"/voting"}> 
+            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
+                <IoNotificationsOutline size={23} className='navicon'/>
+                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
+                    Voting
+                </Text>
+            </Box>
+        </Link>
+
+        <Link to={"/event"}>
+            <Box display={"flex"} flexDir={"column"} alignItems={"center"}>
+                <HiOutlineTrophy size={23} className='navicon'/>
+                <Text fontSize={{base: "13px", lg: "8px"}} lineHeight={{base: 1, lg: 0}}>
+                    Event
+                </Text>
+            </Box>
+        </Link>
+        </Box>
+  )
+}
+
+export default BottomNavContent
