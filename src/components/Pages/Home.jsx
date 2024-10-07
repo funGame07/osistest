6c27886b42bb981ade9b825801349bdf9fd05b1c
elbert <elbertchen007@gmail.com>
2024-10-07 16:19:08 +0700

    aiwdawdind

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -24,6 +24,8 @@ import Carousel from "../carousel/Carousel";
 import { useContext } from "react";
 import { osis } from "../../App";
 import { IoIosArrowBack } from "react-icons/io";
+import { IoMailOutline } from "react-icons/io5";
+import { BsInstagram } from "react-icons/bs";
 
 import "./home.css"
 
@@ -44,7 +46,7 @@ function Home() {
 
 
   return(
-    <Box display={"flex"} flexDir={"column"} alignItems={"center"} px={{base: 0, md: 10, lg: "6%"}} pb={"70px"} pos={"relative"}>
+    <Box display={"flex"} flexDir={"column"} alignItems={"center"} px={{base: 0, md: 10, lg: "6%"}} pos={"relative"}>
       <Image src="schoolbg.png" pos={"absolute"} w={"full"} objectFit={"cover"} h={"100vh"} filter={"auto"} brightness={"0.4"}/>
     
       <Flex pt={{base: "120px", lg: "0"}} minH={"100vh"} flexDir={{base: "column", lg: "row"}} color={"white"}
@@ -76,23 +78,55 @@ function Home() {
         
         
       </Flex>
-
-      {/* <Division text={"Anggota"} colorMode={colorMode} pt={9}/> */}
       
       <Carousel />
 
-      <Flex flexDir={{base: "column", lg:"row"}} w={"100%"} gap={{base: 0, lg: 10}}>
-        <Box w={"90%"} h={"500px"} placeSelf={"start"} roundedRight={"full"} mt={{base: 10, lg: "5em"}} px={5}
-          border={"1px solid rgba(66, 153, 225, 0.5)"} borderStart={"none"} boxShadow={"0 0 20px rgba(66, 153, 225, 0.2)"}>
-            <Heading lineHeight={2}>VISI</Heading>
-            <Text>Menjadi pemimpin dalam inovasi teknologi yang berkelanjutan, menciptakan solusi yang berdampak positif bagi masyarakat dan lingkungan.</Text>
-          </Box>
+      <Division text={""} colorMode={colorMode}/>
 
-          <Box w={"90%"} h={"500px"} placeSelf={"end"} roundedLeft={"full"} mt={{base: 10, lg: "5em"}} px={5}
-          border={"1px solid rgba(66, 153, 225, 0.5)"} borderEnd={"none"} boxShadow={"0 0 20px rgba(66, 153, 225, 0.2)"}>
+      <Flex flexDir={{base: "column", lg:"row"}} w={"100%"} gap={{base: 0, lg: 10}} mt={{base: 10, lg: "5em"}} justifyContent={"space-between"} pb={10}>
+        <Flex w={{base:"90%", lg:"55%"}} h={"500px"} placeSelf={"start"} roundedRight={"full"} px={5} flexDir={"column"}pb={3}
+          border={"1px solid rgba(66, 153, 225, 0.5)"} borderStart={"none"} boxShadow={"0 0 20px rgba(66, 153, 225, 0.2)"} justifyContent={"center"}>
             <Heading lineHeight={2}>VISI</Heading>
-          </Box>
+            <Text w={{base: "70%", lg:"80%"}} fontSize={{lg: "lg"}}>Menjadi pemimpin dalam inovasi teknologi yang berkelanjutan, menciptakan solusi yang berdampak positif bagi masyarakat dan lingkungan.</Text>
 
+            <Heading lineHeight={2}>MISI</Heading>
+            <UnorderedList fontSize={{lg: "lg"}}>
+              <ListItem w={{base: "75%", lg:"80%"}}>menciptakan solusi yang berdampak positif bagi masyarakat dan lingkungan.</ListItem>
+              <ListItem w={{base: "75%", lg:"80%"}}>Menjadi pemimpin dalam inovasi teknologi yang berkelanjutan.</ListItem>
+              <ListItem w={{base: "75%", lg:"80%"}}>menciptakan positif bagi masyarakat dan lingkungan.</ListItem>
+              <ListItem w={{base: "75%", lg:"80%"}}>Menjadi pemimpin dalam inovasi teknologi yang berkelanjutan.</ListItem>
+            </UnorderedList>
+          </Flex>
+
+        <Box w={{base:0, lg: "40%"}} display={{base: "none", lg:"block"}} h={"500px"} rounded={"3xl"} p={4}
+        boxShadow={"0 0 20px rgba(66, 153, 225, 0.1)"} border={"1px solid rgba(66, 153, 225, 0.1)"}>
+          fsfef
+        </Box>
+      </Flex>
+
+      {/* <Division text={""} colorMode={colorMode}/> */}
+
+      <Flex w={"full"} flexDir={"column"} textAlign={"start"} px={5} bg={colorMode == "light" ? "white" : "black"} mt={10} py={5} pb={"5em"} gap={4}>        <Flex flexDir={"column"} gap={1}>
+          <Text className="font-link" fontSize={"lg"} fontWeight={"800"}>HUBUNGI KAMI</Text>
+          <Link style={{display: "flex", alignItems: "center", gap:"1em"}}>
+            <Box as={IoMailOutline} size={"21px"} color={colorMode == "dark"? "blue.600": "blue.700"}/>
+            sultanosis09090@gmail.com
+          </Link>
+        </Flex>
+        <Flex flexDir={"column"} gap={2}>
+          <Text className="font-link" fontSize={"lg"} fontWeight={"800"}>IKUTI KAMI</Text>
+          <Link style={{display: "flex", alignItems: "center", gap:"1em"}}>
+            <Box as={BsInstagram} size={"20px"} color={colorMode == "dark"? "blue.600": "blue.700"}/>
+            @smassa24.25
+          </Link>
+        </Flex>
+
+        <Flex h={"60px"} animation={`${dissapear} 10s ease-in infinite`} alignItems={"center"} mt={3} gap={4} 
+        placeSelf={"center"} justifySelf={"self-end"} opacity={0.5}>
+          <Image src="sultan.png" maxW={"30px"} maxH={"30px"} rounded={"full"}/>
+          <FaExchangeAlt size={20}/>
+          <Image src="logoosis.png" maxW={"40px"} maxH={"40px"}/>
+        </Flex>
       </Flex>
       
 
@@ -101,10 +135,10 @@ function Home() {
 
 
 
-      <Division text={"Lainnya"} colorMode={colorMode} pt={9}/>
+      {/* <Division text={"Lainnya"} colorMode={colorMode} pt={9}/> */}
 
           {/* others */}
-      <Flex gap={3} flexDir={{base: "column", lg: "row"}} minW={"full"}>
+      {/* <Flex gap={3} flexDir={{base: "column", lg: "row"}} minW={"full"}>
         <Flex width={"full"} px={5} gap={1}>
           <Circle bg={"teal"} size={"45px"}>
             <Circle border={`4px solid ${colorMode == "light" ? "white": "black"}`} size={"5em"}>
@@ -197,16 +231,16 @@ function Home() {
         <Image src="sultan.png" maxW={"40px"} maxH={"40px"} rounded={"full"} loading="lazy"/>
         <FaExchangeAlt size={20}/>
         <Image src="logoosis.png" maxW={"50px"} maxH={"50px"} loading="lazy"/>
-      </Flex>
+      </Flex> */}
       
 
     </Box>
   )
 }
 
-function Division({text, colorMode, pt, py=3}){
+function Division({text, colorMode, pt=7}){
   return (
-    <Flex alignItems={"center"} pos={"relative"} minW={"full"} gap={4} px={4} py={py} pt={pt}>
+    <Flex alignItems={"center"} pos={"relative"} minW={"full"} gap={4} px={4} py={3} pt={{base: pt, lg: "5em"}}>
       <Box flexGrow={1} bg={colorMode == "light" ? "#241a0e" : "gray.100"}>
         <StackDivider borderColor='gray.200' width={"full"} minH={"2px"}/>
       </Box>
